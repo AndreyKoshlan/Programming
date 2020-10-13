a5c76fb6c3148a1149bb4b59be9c1656b089a822
AndreyKoshlan <minebobrb@gmail.com>
2020-10-13 22:43:17 +0300

    Task 02: Added variables

--- Practice/02/C++/C++.cpp
+++ Practice/02/C++/C++.cpp
@@ -6,7 +6,12 @@
 int main() {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
-	int a = 3;
-	double b = 3.14;
-	std::cout << a << "\n" << b;
+	int int_a = 3;
+	int int_b = 3.14;
+	double double_a = 3;
+	double double_b = 3.14;
+	std::cout << "(int)A = " << int_a << std::endl;
+	std::cout << "(int)B = " << int_b << std::endl;
+	std::cout << "(double)A = " << double_a << std::endl;
+	std::cout << "(double)B = " << double_b << std::endl;
 }
