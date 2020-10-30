a87da27f5aa7fec38751692e6471e0aca561f670
AndreyKoshlan <minebobrb@gmail.com>
2020-10-30 23:29:24 +0300

    Task 16: First condition fix

--- Practice/16/C++/C++.cpp
+++ Practice/16/C++/C++.cpp
@@ -3,24 +3,37 @@
 
 const std::string pattern = "a???55661";
 
+bool isInNumbers(char s) {
+	return (48 <= int(s) && int(s) <= 57); //0..9  
+}
+
+bool isInLetters(char s) {
+	return (97 <= int(s) && int(s) <= 122); //a..z
+}
+
 bool isInPatternRange(char s) {
-	return ((48 <= int(s) && int(s) <= 57) || //0..9 
-			(97 <= int(s) && int(s) <= 122)); //a..z
+	return (isInNumbers(s)) || //0..9 
+		(isInLetters(s));	   //a..z
 }
 
 bool isCorrectTicket(std::string s) {
 	if (s.length() != pattern.length())
 		return false;
 	bool b = true;
+	int letters = 0, numbers = 0;
 	for (int i = 0; i < pattern.length(); i++) {
 		if (pattern[i] == '?') {
 			b = b and isInPatternRange(s[i]);
 		} else {
 			b = b and (s[i] == pattern[i]);
 		}
+		if (isInLetters(s[i]))
+			letters++;
+		if (isInNumbers(s[i]))
+			numbers++;
 		if (!b) break;
 	}
-	return b;
+	return b and (letters == 2) and (numbers == 7);
 }
 
 int main() {
--- Practice/16/Python/16.py
+++ Practice/16/Python/16.py
@@ -1,20 +1,31 @@
 pattern = "a???55661"
 
+def isInNumbers(c):
+	return ord(c) in range(48, 57+1)
+
+def isInLetters(c):
+	return ord(c) in range(97, 122+1)
+
 def isInPatternRange(c):
-	return (ord(c) in range(48, 57+1)) or\
-		   (ord(c) in range(97, 122+1))
+	return (isInLetters(c)) or\
+		   (isInNumbers(c))
 
 def IsCorrectTicket(s):
 	if len(s) != len(pattern):
 		return False
 	b = True
+	numbers, letters = 0, 0;
 	for i in range(0, len(pattern)):
 		if pattern[i] == "?":
 			b = b and isInPatternRange(s[i])
 		else:
 			b = b and (s[i] == pattern[i])
+		if isInLetters(s[i]):
+			letters += 1;
+		if isInNumbers(s[i]):
+			numbers += 1;
 		if not b: break;
-	return b
+	return b and (letters == 2) and (numbers == 7)
 
 n = int(input('Введите количество билетов N: '))
 s = input('Введите билеты: ').split()
