a9edf07a8fa4d17c2655525746c3bc5b5dd38668
AndreyKoshlan <minebobrb@gmail.com>
2020-12-01 13:34:19 +0300

    Added task 25

---
+++ Practice/25/C++/C++.cpp
@@ -0,0 +1,105 @@
+﻿#include <Windows.h>
+#include <iostream>
+#include <vector>
+#include <stdlib.h>
+#include <time.h> 
+
+void printVector(std::vector<int> v) {
+	for (int i = 0; i < v.size(); i++) {
+		std::cout << v[i] << " ";
+	}
+	std::cout << std::endl;
+}
+
+void inputVector(std::vector<int>& v, int n) {
+	v.resize(0);
+	for (int i = 0; i < n; i++) {
+		int element;
+		std::cin >> element;
+		v.push_back(element);
+	}
+}
+
+std::vector<std::vector<int>> make2dVector(std::vector<int> v) {
+	std::vector<int> tempvec;
+	std::vector<std::vector<int>> rezvec;
+	for (int i = 0; i < v.size(); i++) {
+		tempvec.push_back(v[i]);
+		if ((i+1) % (int)sqrt(v.size()) == 0) {
+			rezvec.push_back(tempvec);
+			tempvec.resize(0);
+		}
+	}
+	return rezvec;
+}
+
+bool isSorted(std::vector<int> v, bool order) {
+	if (v.size() <= 1) 
+		return true;
+	for (int i = 1; i < v.size(); i++) {
+		if (order) {
+			if (v[i] < v[i-1]) return false;
+		} else {
+			if (v[i] > v[i-1]) return false;
+		}
+	}
+	return true;
+}
+
+//BozoSort std::vector<int>
+std::vector<int> BozoSort(std::vector<int> v, bool order = true) {
+	srand(time(NULL));
+	while (!isSorted(v, order)) {
+		int r1 = rand() % v.size();
+		int r2 = rand() % v.size();
+		std::swap(v[r1], v[r2]);
+	}
+	return v;
+}
+
+//BozoSort 2d vector
+std::vector<int> BozoSort(std::vector<std::vector<int>> v2d, bool order = true) {
+	std::vector<int> rezvect;
+	for (int i = 0; i < v2d.size(); i++) {
+		v2d[i] = BozoSort(v2d[i], order);
+		for (int j = 0; j < v2d[i].size(); j++) {
+			rezvect.push_back(v2d[i][j]);
+		}
+	}
+	return rezvect;
+}
+
+//BozoSort 3 values
+std::vector<int> BozoSort(int a, int b, int c, bool order = true) {
+	std::vector<int> v{a, b, c};
+	return BozoSort(v, order);
+}
+
+int main()
+{
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+	int n, i;
+	std::vector<int> v;
+	std::vector < std::vector<int> > v2d;
+    std::cout << "Введите количество чисел: ";
+	std::cin >> n;
+	std::cout << "Введите массив: ";
+	inputVector(v, n);
+	v2d = make2dVector(v);
+
+	std::cout << std::endl;
+	std::cout << "• Отсортированный массив целых чисел: " << "\n\n";
+	printVector(BozoSort(v, true));
+	printVector(BozoSort(v, false));
+	std::cout << "\n\n";
+	
+	std::cout << "• Отсортированный двумерный массив целых чисел: " << "\n\n";
+	printVector(BozoSort(v2d, true));
+	printVector(BozoSort(v2d, false));
+	std::cout << "\n\n";
+
+	std::cout << "• Отсортированные значения трех отдельных чисел: " << "\n\n";
+	printVector(BozoSort(v[0], v[1], v[2], true));
+	printVector(BozoSort(v[0], v[1], v[2], false));
+}
---
+++ Practice/25/Python/task.py
@@ -0,0 +1,63 @@
+import math
+import random
+from multipledispatch import dispatch
+
+def make2dlist(lst):
+	lst2d = []
+	tmplst = []
+	for i in range(len(lst)):
+		tmplst += [lst[i]]
+		if (i+1) % math.sqrt(len(lst)) == 0:
+			lst2d += [tmplst]
+			tmplst = []
+	return lst2d 
+
+def isSorted(lst, order):
+	if len(lst) <= 1:
+		return True;
+	for i in range(1, len(lst)):
+		if order:
+			if (lst[i] < lst[i-1]): return False
+		else:
+			if (lst[i] > lst[i-1]): return False
+	return True
+
+@dispatch(list, bool) 
+def BozoSort(lst, order):
+	if type(lst[0]) != list:
+		while not (isSorted(lst, order)):
+			r1 = random.randint(0, len(lst)-1)
+			r2 = random.randint(0, len(lst)-1)
+			lst[r1], lst[r2] = lst[r2], lst[r1]
+		return lst
+	else:
+		rezlst = []
+		for i in range(len(lst)):
+			rezlst.extend(BozoSort(lst[i], order))
+		return rezlst
+
+
+@dispatch(int, int, int, bool)
+def BozoSort(a, b, c, order):
+	return BozoSort([a, b, c], order)
+
+lst = list(map(int, input('Введите массив: ').split()))
+lst2d = make2dlist(lst)
+
+original_lst = []
+original_lst.extend(lst)
+print(original_lst)
+print('• Отсортированный массив целых чисел: \n')
+print(BozoSort(lst, True))
+print(BozoSort(lst, False))
+print('\n')
+
+print('• Отсортированный двумерный массив целых чисел: \n')
+print(BozoSort(lst2d, True))
+print(BozoSort(lst2d, False))
+print('\n')
+
+lst = original_lst
+print('• Отсортированные значения трех отдельных чисел: \n')
+print(BozoSort(lst[0], lst[1], lst[2], True))
+print(BozoSort(lst[0], lst[1], lst[2], False))
