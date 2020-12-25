719ba1305886d1327137510abf07721c6ef5aa09
AndreyKoshlan <minebobrb@gmail.com>
2020-12-25 23:52:37 +0300

    Added task 30

---
+++ Practice/30/C++/C++.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+struct Item {
+	enum {
+		COIN,
+		RUNE,
+		WEAPON,
+		ARMOR
+	} ItemType;
+	union {
+		struct {
+			int count;
+		} Coin;
+		struct {
+			int level;
+			enum {
+				FIRE, WATER, EARTH, AIR
+			} element;
+		} Rune;
+		struct {
+			int damage;
+			int critical;
+			int durability;
+		} Weapon;
+		struct {
+			int guard;
+			int durability;
+		} Armor;
+	} Item;
+};
+
+int main()
+{
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+	Item LootBox;
+}
