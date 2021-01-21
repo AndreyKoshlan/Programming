f98a1f584f17d03d6858667e298080a3111a33f4
AndreyKoshlan <minebobrb@gmail.com>
2021-01-21 17:21:01 +0300

    Task 08: Change equation solver

--- Practice/08/Python/08.py
+++ Practice/08/Python/08.py
@@ -1,7 +1,20 @@
 while 1:
 	try:
-		print(eval(input('Введите выражение: ')))
-		break;
+		a, op, b = input('Введите выражение: ').split()
+		a, b = map(float, (a, b))
+		if op == '+':
+			print(a+b)
+			break
+		if op == '-':
+			print(a-b)
+			break
+		if op == '*':
+			print(a*b)
+			break
+		if op == '/':
+			print(a/b)
+			break
+		raise
 	except ZeroDivisionError:
 		print('Деление на "0" невозможно')
 	except Exception:
